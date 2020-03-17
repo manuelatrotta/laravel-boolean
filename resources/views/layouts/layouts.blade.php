25626237197db09b550787b68da66e399a0f6c0d
manuelatrotta <manuelat88@hotmail.it>
2020-03-17 17:20:18 +0100

    introduzione header e footer

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,13 @@
+    @include('layouts.partials._header')
+      <header>
+        @yield('header')
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+
+    @include('layouts.partials._footer')
