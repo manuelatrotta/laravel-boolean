610c2eace5c2e4328e1fe9a95859dfe8d81334df
manuelatrotta <manuelat88@hotmail.it>
2020-03-17 16:39:57 +0100

    creazione controller

---
+++ app/Http/Controllers/StaticPage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPage extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function privacy()
+    {
+
+    }
+
+    public function faq()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Student extends Controller
+{
+  public function index()
+  {
+
+  }
+
+  public function show()
+  {
+
+  }
+}
