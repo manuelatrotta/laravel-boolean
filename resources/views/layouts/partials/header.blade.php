a631c12664d1ad807a71141519b096179e14a95d
manuelatrotta <manuelat88@hotmail.it>
2020-03-17 18:56:19 +0100

    modifica header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -10,7 +10,7 @@
         <ul>
           <li><a href="{{route('static_page.home')}}">Home</a></li>
           <li><a href="#">Corso</a></li>
-          <li><a href="#">Dopo Corso</a></li>
+          <li><a href="{{route('student.index')}}">Dopo Corso</a></li>
           <li><a href="#">Lezione Gratuita</a></li>
           <li><a href="#">Candidati Ora</a></li>
         </ul>
