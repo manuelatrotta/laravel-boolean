b0fbdbff233b85136e5cff0bf885a7e75da5c762
manuelatrotta <manuelat88@hotmail.it>
2020-03-18 08:50:09 +0100

    inseerimento funzione per students

--- app/Http/Controllers/Student.php
+++ app/Http/Controllers/Student.php
@@ -4,15 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Student extends Controller
+class StudentController extends Controller
 {
-  public function index()
-  {
+    private $students;
 
-  }
+    public function __construct()
+    {
+        $this->getStudents();
+    }
+    public function index()
+    {
+        $students = $this->students;
+        // $students = config('students.students');
+        // dd(config('students.students'));
+        return view('student.index',compact('students'));
+    }
 
-  public function show()
-  {
+    public function show($id)
+    {//se non esiste la key allora la pagina si blocca con abort
+        if (!array_key_exists($id, $this->students)) {
+            abort('404');
+        };
+        $student = $this->students[$id];
+        return view('student.show',compact('student'));
+    }
 
-  }
+    //funzione che richiama la $private students
+    public function getStudents()
+    {
+        $this->students = config('students.students');
+        // dd(config('students.students'));
+    }
 }
