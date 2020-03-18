c6f820128d55882684f7a694e794320024719cde
manuelatrotta <manuelat88@hotmail.it>
2020-03-18 16:20:13 +0100

    sitemazione array students

---
+++ config/students.php
@@ -0,0 +1,37 @@
+<?php
+//passo l'array che era in StudentController 
+return [
+   'students' => [
+            [
+                'img' => 'https://www.topolino.it/wp-content/uploads/2019/12/pippointera.png',
+                'name' => 'Pippo',
+                'age' => 30,
+                'company' => 'Disney',
+                'description' => 'lorem ipsum',
+                'role' => 'Web Developer',
+                'gender' => 'm',
+                'slug' => 'pippo'
+            ],
+            [
+                'img' => 'https://www.cartonionline.com/immagini/topolino/topolino.jpg',
+                'name' => 'Topolino',
+                'age' => 50,
+                'company' => 'Disney',
+                'description' => 'lorem ipsum',
+                'role' => 'Backend Developer',
+                'gender' => 'm',
+                'slug' => 'topolino'
+            ],
+            [
+                'img' => 'https://wips.plug.it/cips/supereva/cms/2019/07/104750479_l.jpg?w=850&a=r',
+                'name' => 'Minnie',
+                'age' => 50,
+                'company' => 'Disney',
+                'description' => 'lorem ipsum',
+                'role' => 'Frontend Developer',
+                'gender' => 'f',
+                'slug' => 'minnie'
+            ]
+    ],
+    'genders' => ['m', 'f', 'all']
+  ];
