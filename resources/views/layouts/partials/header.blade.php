f7cc62ab7884936e7d723748420a188c27760f72
manuelatrotta <manuelat88@hotmail.it>
2020-03-18 19:07:15 +0100

    sistemazione header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,17 +1,42 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <div class="container">
-      <nav>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  //asset helper che genera url completo e prende la cartella richiesta
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+{{-- @dd(Request::route()->getName()) --}}
+
+<body>
+  <div class="container">
+    {{-- header --}}
+    <header>
+      <nav class="navbar">
+        <div class="logo">
+          Boolean
+        </div>
         <ul>
-          <li><a href="{{route('static_page.home')}}">Home</a></li>
-          <li><a href="#">Corso</a></li>
-          <li><a href="{{route('student.index')}}">Dopo Corso</a></li>
-          <li><a href="#">Lezione Gratuita</a></li>
-          <li><a href="#">Candidati Ora</a></li>
+          <li>
+            <a class="" href="{{route('static_page.home')}}">Home</a>
+          </li>
+          <li>
+            <a href="">Corso</a>
+          </li>
+          <li>
+            <a class="" href="{{route('student.index')}}">Dopo Corso</a>
+          </li>
+          <li>
+            <a href="">Lezione Gratuita</a>
+          </li>
+          <li>
+            <a href="">Candidati ora</a>
+          </li>
         </ul>
       </nav>
+      @yield('header')
+    </header>
+    {{-- /header --}}
