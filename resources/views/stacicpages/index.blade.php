bbc132a2ab95a69b846f1c79a1554e453d98f338
manuelatrotta <manuelat88@hotmail.it>
2020-03-21 11:17:45 +0100

    creazione jumbo in index.blade della static page

--- resources/views/stacicpages/index.blade.php
+++ resources/views/stacicpages/index.blade.php
@@ -1,5 +1,28 @@
-@extends('layouts.layout')
 
-@section('header')
-  pippo
+@extends('layouts.layout')
+@section('jumbo')
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <div class="container bg-white">
+              <div class="row">
+                <div class="col-6">
+                  <h2>DIVENTA <br>
+                    <strong>SVILUPPATORE WEB</strong>
+                  </h2>
+                  <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                  <ul>
+                    <li>6 mesi di corso intensivo online in diretta</li>
+                    <li> Nessuna competenza di programmazione richiesta</li>
+                    <li>Siamo certi del tuo successo, altrimenti ti rimborsiamo</li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <img class="img-fluid" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
